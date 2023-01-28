7c9df92d28d324e8fba6b800a1b1380831e88baf
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-28 11:12:30 +0600

    [ADD] variable and basic rules and recap previous lesson

---
+++ variable.js
@@ -0,0 +1,34 @@
+// number type variable.
+var sunglassPrice = 120;
+
+// string type variable.
+var specialOneName = "Ariyana Grande";
+
+// boolean type variable.
+var isSingle = true;
+
+// two types of number variable:
+// Integer: 120, 
+// Float, decimal: 120.5
+
+
+// variable naming convenstio: 
+/*
+* 1. single word
+* 2. no gap or space
+* 3. no quote
+* 4. no keyword
+* 5. cann't start with a number
+* 6. should use camelCase
+*/
+
+
+// Math operations: +, -, *, /, %
+// shorthand: +=, -=, *=, /=, %=, ++, --
+
+
+// convert: parseInt(), parseFloat(), toString()
+
+// toFixed(): convert float to string with fixed decimal point
+
+// reminder: %, modulus operator
