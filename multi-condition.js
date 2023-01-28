b74dd86dd8df419b207374cfc51fa0a6421b6dc2
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-28 12:47:18 +0600

    [ADD] multi condition

---
+++ multi-condition.js
@@ -0,0 +1,29 @@
+// var isGraduate = true;
+// var salary = 250000;
+// var cars = 1;
+
+// if (isGraduate == true) {
+//   console.log("Esho biye kore feli..");
+// } else {
+//   console.log("kopale biya nai");
+// }
+
+
+// if (isGraduate == true && salary > 50000 && cars >= 1) {
+//   console.log("Esho biye kore feli..");
+// } else {
+//   console.log("kopale biya nai");
+// }
+
+var isGraduate = true;
+var salary = 250000;
+var cars = 1;
+
+
+if (isGraduate == true || salary >= 50000 || cars >= 1) {
+  console.log("Esho biye kore feli..");
+} else {
+  console.log("kopale biya nai");
+}
+   
+
