79faf88784fa43ecd148feff83b6e22c7d2d994d
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-28 12:02:27 +0600

    [ADD] Array.pop() Array.push() Array.shift() Array.unshift()

---
+++ push-pop.js
@@ -0,0 +1,25 @@
+var numbers = [78, 45, 98, 45];
+
+// 1. use push to add element to an array as the last element array.
+// numbers.push(63);
+// console.log(numbers);
+
+var friends = [`Balam`, `Kalam`, `Salam`];
+friends.push(`galam`);
+friends.push(`pailam`);
+// console.log(friends);
+
+// use pop to remove the last element of an array
+
+// var element = numbers.pop();
+// console.log(element);
+
+
+// use shift to remove the first element of an array
+// var element = numbers.shift();
+// console.log(element);
+// console.log(numbers);
+
+// use unshift to add element to an array as the first element of an array
+numbers.unshift(55);
+console.log(numbers);
