c1c4d2eadf359c2c1334ab30cbe80f907a9651d8
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-28 11:44:38 +0600

    [ADD] index of array

---
+++ index.js
@@ -0,0 +1,17 @@
+var numbers = [45, 68, 78, 56, 89, 98];
+
+
+// 1. get element by index 
+var element = numbers;
+
+// 2. set element valu by index
+numbers[1] = 77;
+numbers[3] = 44;
+
+// console.log(element);
+
+// 3. find index of an element
+var positionIndex = numbers.indexOf(189)
+
+
+console.log(positionIndex);
