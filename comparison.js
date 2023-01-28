3e9c70b5796d4435b400406d4b5a3102dbe66c7b
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-28 12:14:46 +0600

    [ADD] comparison

---
+++ comparison.js
@@ -0,0 +1,32 @@
+console.log("====================================");
+// console.log(5 < 6);
+// console.log(5 > 6);
+// console.log(5 == 6);
+// console.log(6 == 6);
+//  console.log(5 !== 6);
+//  console.log(15 !== 15);
+
+// less than or equal to
+// console.log(5 <= 6);
+// console.log(16 <= 6);
+
+
+// gater than or equal to
+// console.log(5 >= 6);
+// console.log(5 >= 2);
+// console.log(2 >= 2);
+
+var num1 = 56;
+var num2 = 89;
+
+// console.log(num1 < num2);
+// console.log(num1 > num2);
+// console.log(num1 == num2);
+// console.log(num1 != num2);
+// console.log(num1 <= num2);
+console.log(num1 >= num2);
+
+// multiple condition
+
+
+console.log("====================================");
