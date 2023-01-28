375c97b04211e0b816f13314c92549d24b74a34a
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-28 15:58:48 +0600

    [ADD] multi-stage-condition.js

---
+++ multi-stage-condition.js
@@ -0,0 +1,15 @@
+var money = 50;
+
+var danishPrice = 45;
+var butterBread = 35;
+var tostBiscuit = 20;
+
+if (danishPrice < money) {
+   console.log('Danish khabo');
+} else if (butterBread < money) {
+   console.log('Butter Bread khabo');
+}  else if (tostBiscuit < money) {
+   console.log('Tost Biscuit khabo');
+} else {
+   console.log(`khali cha sasther jonno valo`);
+}
