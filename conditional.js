81c856b9750a3b745cf7727394a9fc6b0967ca15
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-28 12:32:05 +0600

    [ADD] conditional

---
+++ conditional.js
@@ -0,0 +1,32 @@
+var iphonePrice = 79000;
+var myBudget = 95000;
+
+/* 
+if iPhone price is less than my budget 
+    i'll buy iPhone. 
+
+    if(condition) {
+    will execute if condition is true.
+    }
+
+*/
+
+if(iphonePrice < myBudget) {
+    console.log("iPhone diye futani marbo");
+}
+
+var chickenPrice = 500;
+var myMoney = 50;
+
+// if (chickenPrice <= myMoney) {
+//     console.log("Yes! Murgir Ran khamu and haddi chibamu !!!");
+// }else if(chickenPrice>myMoney){
+//     console.log("Smashed potato with lentil soup");
+// }
+   
+
+if (chickenPrice < myMoney) {
+   console.log("Yes! Murgir Ran khamu and haddi chibamu !!!");
+}else{
+    console.log("Smashed potato with lentil soup");
+}
