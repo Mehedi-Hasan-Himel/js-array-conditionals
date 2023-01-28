6e301646ffa82882a4b6f9005032f65da5d980e3
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-28 16:03:20 +0600

    [ADD] nested conditions

---
+++ nested-condition.js
@@ -0,0 +1,15 @@
+var math = true;
+var geometry = true;
+var straightLine = false;
+
+if (math == true) {
+  if (geometry == treu) {
+    if (straightLine == true) {
+    } else {
+      console.log(`baka pothe colba na`);
+    }
+  } else {
+    console.log(`Pithagoras hoite parba na`);
+  }
+} else {
+}
